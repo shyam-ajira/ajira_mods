d2170bca92ceed0aeb808e1de1029611a415ac02
Deepak Mahaseth <shyam.s.ajira@gmail.com>
2024-12-16 21:21:52 +0545

    complete work on UI

---
+++ ajira_mods/ajira_mods/doctype/municipality/municipality.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ajira and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Municipality(Document):
+	pass
